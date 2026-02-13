1c10c2b9bda2e50e63241df4a8483e7d70af9a2a
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2026-02-13 16:30:17 +0800

    Add solution for rnn-step-forward

---
+++ rnn-step-forward/rnn-step-forward.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
+    """
+    Returns: h_t of shape (H,)
+    """
+    # Write code here
+    forward = np.tanh(x_t @ Wx + h_prev @ Wh + b)
+    h_prev = forward 
+    return forward 
